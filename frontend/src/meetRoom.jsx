4270af24a4b8352c740ab91d7780524f8e602752
Chandraprakash Pandey <cp.o.pandey@gmail.com>
2026-03-01 02:04:14 +0530

    Added joint meet Mssg

--- frontend/src/meetRoom.jsx
+++ frontend/src/meetRoom.jsx
@@ -22,6 +22,18 @@ export default function MeetRoom() {
         </form>
       </div>
     </div>)}
+    {submitted && (<div className='h-screen flex justify-center items-center flex-col gap-10 bg-gray-100'>
+      <div>
+        <h1 className='text-5xl font-bold'>Welcome, {name}!</h1>
+        <p className='text-2xl mt-5'>You have successfully joined the meeting.</p>
+      </div>
+      <div>
+        <button className='bg-blue-500 text-white px-4 py-2 rounded-lg cursor-pointer'>toggle Video</button> 
+      <button className='bg-blue-500 text-white px-4 py-2 rounded-lg cursor-pointer'>toggle Audio</button>
+      <button className='bg-blue-500 text-white px-4 py-2 rounded-lg cursor-pointer'>Chatting</button>
+      <button className='bg-red-500 text-white px-4 py-2 rounded-lg cursor-pointer'>Leave Meeting</button>
+      </div>
+    </div>)}
     </>
   )
 }
